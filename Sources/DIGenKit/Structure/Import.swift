fef62db38e63c1cd6b1c2a607047fb687eef56d2
Rerurate_514 <71661237+Rerurate514@users.noreply.github.com>
2025-02-07 11:39:25 +0900

    Update Import.swift

--- Sources/DIGenKit/Structure/Import.swift
+++ Sources/DIGenKit/Structure/Import.swift
@@ -14,20 +14,20 @@ struct Import {
     static func imports(from file: File) throws -> [Import] {
         let syntaxMap = try SyntaxMap(file: file)
         let importTokenIndices = syntaxMap.tokens.enumerated()
-            .compactMap { index, token -> Int? in
+          .compactMap { index, token -> Int? in
                 guard token.type == "source.lang.swift.syntaxtype.keyword" else {
                     return nil
                 }
 
                 let view = file.contents.utf8
-                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset))
-                let endIndex = view.index(startIndex, offsetBy: Int(token.length))
+                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset.value)) // 修正
+                let endIndex = view.index(startIndex, offsetBy: Int(token.length.value)) // 修正
                 let value = String(view[startIndex..<endIndex])!
-                return value == "import" ? index : nil
+                return value == "import"? index: nil
             }
 
         let importedModuleNames = importTokenIndices
-            .compactMap { index -> String? in
+          .compactMap { index -> String? in
                 let identifierIndex = index + 1
                 guard identifierIndex < syntaxMap.tokens.count else {
                     return nil
@@ -39,8 +39,8 @@ struct Import {
                 }
 
                 let view = file.contents.utf8
-                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset))
-                let endIndex = view.index(startIndex, offsetBy: Int(token.length))
+                let startIndex = view.index(view.startIndex, offsetBy: Int(token.offset.value)) // 修正
+                let endIndex = view.index(startIndex, offsetBy: Int(token.length.value)) // 修正
                 return String(view[startIndex..<endIndex])!
             }
 
