110593622a8be0812069a6f44b8b45ee8ab95190
Rerurate_514 <71661237+Rerurate514@users.noreply.github.com>
2025-02-07 11:36:37 +0900

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -2,23 +2,23 @@
 import PackageDescription
 
 let package = Package(
-  name: "DIKit",
-  platforms: [
-      .macOS(.v12), .iOS(.v9), .tvOS(.v9), .watchOS(.v2)
-  ],
-  products: [
-    .executable(name: "dikitgen", targets: ["dikitgen"]),
-    .library(name: "DIKit", targets: ["DIKit"]),
-    .library(name: "DIGenKit", targets: ["DIGenKit"])
-  ],
-  dependencies: [
-    .package(url: "https://github.com/Rerurate514/SourceKitten.git", .exact("0.36.1"))
-  ],
-  targets: [
-    .target(name: "DIKit"),
-    .target(name: "DIGenKit", dependencies: ["DIKit", "SourceKittenFramework"]),
-    .target(name: "dikitgen", dependencies: ["DIGenKit"]),
-    .testTarget(name: "DIGenKitTests", dependencies: ["DIGenKit"])
-  ],
-  swiftLanguageVersions: [.v5]
+    name: "DIKit",
+    platforms: [
+      .macOS(.v12),.iOS(.v9),.tvOS(.v9),.watchOS(.v2)
+    ],
+    products: [
+      .executable(name: "dikitgen", targets: ["dikitgen"]),
+      .library(name: "DIKit", targets: ["DIKit"]),
+      .library(name: "DIGenKit", targets: ["DIGenKit"])
+    ],
+    dependencies: [
+      .package(url: "https://github.com/Rerurate514/SourceKitten.git",.exact("0.36.1"))
+    ],
+    targets: [
+      .target(name: "DIKit"),
+      .target(name: "DIGenKit", dependencies: ["DIKit",.product(name: "SourceKittenFramework", package: "SourceKitten")]),
+      .executableTarget(name: "dikitgen", dependencies: ["DIGenKit"]),
+      .testTarget(name: "DIGenKitTests", dependencies: ["DIGenKit"])
+    ],
+    swiftLanguageVersions: [.v5]
 )
